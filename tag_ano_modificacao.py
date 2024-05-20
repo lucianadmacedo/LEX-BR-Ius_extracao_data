afd26f1563868ac842ab790b88c783ec275c3118
Luciana Dias de Macedo <lucianadiasdemacedo@MacBook-Pro-Luciana.local>
2024-05-20 18:14:28 -0400

    fix script and remove duplicated texts

--- tag_ano_modificacao.py
+++ tag_ano_modificacao.py
@@ -19,6 +19,7 @@ def process_xml(xml_file_path):
     # Parse the XML file
     tree = etree.parse(xml_file_path)
 
+    all_texts = [ ]
     # Iterate through the XML elements
     for modificacao_elem in tree.xpath(".//modificacao"):
         year = extract_year(modificacao_elem.text)
@@ -35,15 +36,26 @@ def process_xml(xml_file_path):
 
             print(f"Renamed tag: {new_tag_name}")
             parent = modificacao_elem.getparent()
+
+
             all_text = ''.join(etree.tostring(child, encoding='unicode', method='text') for child in parent.iter())
             all_text = all_text.split(modificacao_elem.text)[0].strip().split('\n')[-1]
 
             modificacao_elem.text = all_text + modificacao_elem.text
+            all_texts.append(all_text)
 #            print(modificacao_elem.text)
 
             # Insert the new tags into the XML tree
     # Write the modified XML back to file
-    tree.write("modified_file.xml", encoding="UTF-8", xml_declaration=True)
+    txt = etree.tostring(tree, encoding = 'unicode')
+    for i in all_texts:
+        print(i)
+        txt = txt.replace(i, '', 1)
+#    print(txt)
+#    print(etree.tostring(tree))
+    with open('modified_file.xml','w') as f:
+        f.write(txt)
+#    tree.write("modified_file.xml", encoding="UTF-8", xml_declaration=True)
 
 # Replace 'your_file.xml' with the actual path to your XML file
 xml_file_path = 'your_file.xml'
