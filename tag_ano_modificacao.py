8f0bad43dc505b0ae4899453e3855d2d6d0d9171
Luciana de Macedo <lucianadiasdemacedo@gmail.com>
2025-01-14 01:18:40 +0000

    fix script to include texts there were being left out

--- tag_ano_modificacao.py
+++ tag_ano_modificacao.py
@@ -18,33 +18,23 @@ def extract_year(modificacao_text):
 def process_xml(xml_file_path):
     # Parse the XML file
     tree = etree.parse(xml_file_path)
-
     all_texts = [ ]
     # Iterate through the XML elements
-    for modificacao_elem in tree.xpath(".//modificacao"):
+    for modificacao_elem in tree.xpath(".//modificacao"): # Probably it will be necessary to check cases
         year = extract_year(modificacao_elem.text)
         print(year)
         if year is not None:
             # Create new tags with the year appended to their names
             new_tag_name = f"modificacao_{year}"
-
-
-
             # Rename the original modificacao tags
             modificacao_elem.tag = new_tag_name
-
-
             print(f"Renamed tag: {new_tag_name}")
             parent = modificacao_elem.getparent()
-
-
             all_text = ''.join(etree.tostring(child, encoding='unicode', method='text') for child in parent.iter())
             all_text = all_text.split(modificacao_elem.text)[0].strip().split(r'\n')[-1]
-
             modificacao_elem.text = all_text + modificacao_elem.text
             all_texts.append(all_text)
 #            print(modificacao_elem.text)
-
             # Insert the new tags into the XML tree
     # Write the modified XML back to file
     txt = etree.tostring(tree, encoding = 'unicode')
@@ -57,6 +47,34 @@ def process_xml(xml_file_path):
         f.write(txt)
 #    tree.write("modified_file.xml", encoding="UTF-8", xml_declaration=True)
 
+def get_year(bpart):
+    yearpart = bpart.split('</modificacao>')[0]
+    return (extract_year(yearpart.strip()), yearpart)
+
+
+def get_previous_text(text):
+    return text.split('>')[-1]
+
+def get_text(xml_file_path):
+    f2 = open(xml_file_path + 'nu', 'w')
+    with open(xml_file_path, 'r') as f:
+        text = f.read()
+    modtext = text.split('<modificacao>')
+    for i in range(len(modtext) - 1):
+        write_text(f2, modtext[i], get_year(modtext[i+1]))
+
+
+def write_text(f2, pretext, year):
+    if year[0] is None:
+        f2.write(pretext + '<modificacao>' + year[1] + '</modificacao>')
+        return 0
+    prepretext = get_previous_text(pretext)
+    pretext = pretext.split(prepretext)[0]
+    pretext = pretext.split('</modificacao>')[-1]
+    f2.write(pretext + '\n<modificacao_' + year[0] + '>' + prepretext + year[1] +  '</modificacao_' + year[0] + '>\n')
+
 # Replace 'your_file.xml' with the actual path to your XML file
 xml_file_path = 'your_file.xml'
-process_xml(xml_file_path)
+#process_xml(xml_file_path)
+
+get_text(xml_file_path)
